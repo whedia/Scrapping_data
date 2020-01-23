ade6161941f7c2eacd2d11f3f43291d22bb529b4
whedia <hedia.wejdi94@gmail.com>
2020-01-23 21:21:16 +0100

    second commit

--- lib/dark_tradder.rb
+++ lib/dark_tradder.rb
@@ -1,13 +1,42 @@
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 
-crypto_names = page.css('//tr/a[@class="cmc-link"]')
-puts crypto_names
+def crypto_name_array()
 
+  page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+  
+  cryptos_names = []
+  cryptos_values = []
 
-crypto_currency = page.xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div[2]/div[2]/div[3]/div/table/tbody/tr[1]/td[4]/a')
+  cryptos = page.xpath('//tr/td[3]/div')
+  values = page.xpath('//tr/td[5]/a')
+  cryptos.each{|val|
+    txt = "#{val.text}"
+    cryptos_names.push(txt)
+  }
+  values.each{|val|
+    txt = "#{val.text}".sub(",", "")
+    txt[0]=''
+    cryptos_values.push(txt.to_f)
+  }
+
+  result = Hash[cryptos_names.zip cryptos_values]
+  result
+end
+
+def proper_format (hash)
+  a = []
+  hash.each{|key, val|
+  h = Hash.new
+  h[key] = val
+  a.push(h)
+  }
+
+  return a
+end
+
+p proper_format(crypto_name_array)
 
 
 
--- lib/marie_christmas.rb
+++ lib/marie_christmas.rb
@@ -1,18 +1,50 @@
-require 'nokogiri'
 require 'open-uri'
+require 'nokogiri'
 
-page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
-
-puts townhall_url = page.xpath('//a')
-
-
-=begin
 def get_townhall_email(townhall_url)
-    
+  page = Nokogiri::HTML(open(townhall_url))
+  tds = page.xpath('//td')
+  array = []
+  
+#Exception =>
+
+  begin
+    array = tds.select{|td| td.text =~ /.+@.+\.\w+/}
+  rescue => exception
   end
+  
+  if array.length > 0
+    return array[0].text.strip
+  else
+    return ""
+  end
+  
 end
 
-def get_townhall_urls
+def get_townhalls_urls
 
+  links = {}
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+  table = page.xpath('//a[@class="lientxt"]')
+  table.each{ |link|
+    
+     name = link.text
+     lnk = link.xpath('@href').to_s
+     lnk_formatted = lnk.sub('.', 'http://annuaire-des-mairies.com')
+     links[name.downcase.strip.gsub(' ', '_')] = lnk_formatted
+    }
+    links
 end
-=end
+
+def get_all_emails
+  emails = []
+  townhalls_urls = get_townhalls_urls()
+  townhalls_urls.each{|key, value|
+    mail = get_townhall_email(value)
+    h = Hash.new
+    h[key] = mail
+    emails.push(h)
+  }
+  return emails
+end
+p get_all_emails
